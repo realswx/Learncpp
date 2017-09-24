007ecdb755770fbde69a551fce2fd47115cd3da8
realswx <sangwx23@hotmail.com>
2017-09-24 10:03:37 +0800

    练习6.55

---
+++ 6/练习6.55.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef int fun(int, int);
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+int reduce(int a, int b)
+{
+    if(a >= b)
+        return a - b;
+    else
+        return b - a;
+}
+int ride(int a, int b)
+{
+    return a * b;
+}
+int divide(int a, int b)
+{
+    return a / b;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    vector<fun*> ivec;
+    ivec.push_back(sum);
+    ivec.push_back(reduce);
+    ivec.push_back(ride);
+    ivec.push_back(divide);
+    for(auto it = ivec.begin(); it != ivec.end(); ++it) {
+        cout << (*it)(a, b) << endl;
+    }
+    return 0;
+}
