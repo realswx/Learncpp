3bc81fcff03479161be8bb5ea657a0aa347d5bd5
realswx <sangwx23@hotmail.com>
2017-09-25 11:06:22 +0800

    7.1.2节练习

---
+++ 7/ex7.4.h
@@ -0,0 +1,14 @@
+#ifndef EX7_4_H_INCLUDED
+#define EX7_4_H_INCLUDED
+
+#include<string>
+using namespace std;
+
+class Person
+{
+    string name;
+    string address;
+};
+
+
+#endif // EX7_4_H_INCLUDED
---
+++ 7/ex7_5.h
@@ -0,0 +1,23 @@
+#ifndef EX7_5_H_INCLUDED
+#define EX7_5_H_INCLUDED
+
+#include<string>
+using namespace std;
+
+class Person
+{
+    string name;
+    string address;
+
+public:
+    string& getName() const {
+        return name;
+    }
+    string&getAddress() const {
+        return address;
+    }
+};
+
+
+
+#endif // EX7_5_H_INCLUDED
