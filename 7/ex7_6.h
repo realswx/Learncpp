3c19df496c845b88a901ddcdc06bcbc0bf5b2382
realswx <sangwx23@hotmail.com>
2017-09-26 19:54:05 +0800

    ex7.6

--- 7/ex7_6.h
+++ 7/ex7_6.h
@@ -22,5 +22,16 @@ Sales_data& Sales_data::combine(const Sales_data& rhs) {
     revenue += rhs.revenue;
     return *this;
 }
+istream& read(istream& is,Sales_data& item) {
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+Sales_data add(const Sales_data& lhs, const Sales_data& rhs) {
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
 
 #endif // EX7_6_H_INCLUDED
