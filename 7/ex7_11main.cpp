e5f862014a9870a90be23ca968848a22e2f0a492
realswx <sangwx23@hotmail.com>
2017-09-27 22:41:35 +0800

    ex7_11

---
+++ 7/ex7_11main.cpp
@@ -0,0 +1,18 @@
+#include"ex7_11.h"
+using namespace std;
+
+int main()
+{
+    Sales_data item1;
+    print(cout, item1) << endl;
+
+    Sales_data item2("12345");
+    print(cout, item2) << endl;
+
+    Sales_data item3("12345",3,20.50);
+    print(cout, item3) << endl;
+
+    /*Sales_data item4(cin);
+    print(cout, item4) << endl;*/
+    return 0;
+}
