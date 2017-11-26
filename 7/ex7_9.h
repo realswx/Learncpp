0bc9ceb878e91ae28dc964bd8b62abc65bc6cc69
realswx <sangwx23@hotmail.com>
2017-11-26 10:05:31 +0800

    update

--- 7/ex7_9.h
+++ 7/ex7_9.h
@@ -1,32 +1,34 @@
 #ifndef EX7_9_H_INCLUDED
 #define EX7_9_H_INCLUDED
-
 #include<iostream>
 #include<string>
+
 using namespace std;
 
 class Person
 {
     string name;
     string address;
+
 public:
-    const string& getName() const {
+    const string &getName() const {
         return name;
     }
-    const string& getAddress() const {
+    const string &getAddress() const {
         return address;
     }
+
 };
 
-istream& read(istream& is, Person& person) {
+istream &read(istream &is, Person &person) {
     is >> person.name >> person.address;
     if (!is)
         person = Person();
     return is;
 }
 
-ostream& print(ostream& os, const Person& person) {
-    os << person.name << " " << person.address;
+ostream &print(ostream &os, const Person &person) {
+    os << person.name << person.address;
     return os;
 }
 
